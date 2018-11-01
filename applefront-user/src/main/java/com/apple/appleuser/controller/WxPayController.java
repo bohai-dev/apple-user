55d5625a6aeee4a22facae886f6de1a891de4c73
caoxingyu <caoxingyu2016@qq.com>
2018-11-01 10:30:56 +0800

    微信支付异步通知

--- applefront-user/src/main/java/com/apple/appleuser/controller/WxPayController.java
+++ applefront-user/src/main/java/com/apple/appleuser/controller/WxPayController.java
@@ -17,6 +17,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -27,6 +28,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
 @RequestMapping("/wxpay")
 public class WxPayController {
 
@@ -140,6 +142,8 @@ public class WxPayController {
 
         } catch (Exception e) {
             LOGGER.error("微信支付接收通知发生错误：" + e.toString());
+        }finally {
+            out.close();
         }
 
     }
